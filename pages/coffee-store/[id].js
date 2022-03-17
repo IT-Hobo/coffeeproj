ba30c85c501c641f0afa40420a967b25f68c9dc4
JesseZimmer <jzimmer814@gmail.com>
2022-03-17 00:20:16 -0400

    Initial Commit

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+const CoffeeStore = () => {
+    const router = useRouter();
+    console.log('router', router);
+    return (
+    
+    <div>
+        Coffee Store Page {router.query.id}
+        <Link href="/">
+            <a>Back to home</a>
+        </Link>
+    </div>
+    
+    );
+};
+
+export default CoffeeStore;
+
+//On lesson 41 of react course
